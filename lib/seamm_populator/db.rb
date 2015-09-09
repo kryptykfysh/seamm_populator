a2c343e1b954657dca21170533d2f778bcadb66b
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2015-09-10 07:20:13 +1000

    Updated model.

---
+++ lib/seamm_populator/db.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+require 'active_record'
+require 'pg'
+require 'yaml'
+
+module SeammPopulator
+  class Db < ActiveRecord::Base
+    @config = YAML.load_file(
+      File.join(
+        File.dirname(__FILE__),
+        '../../database.yml'
+      )
+    )
+
+    class << self; attr_reader :config; end;
+
+    self.abstract_class = true
+    establish_connection @config
+  end
+end
